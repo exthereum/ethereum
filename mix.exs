5740efa25c7c26d1005ff69b84760a841d1e0fa2
Antoine Toulme <antoine@lunar-ocean.com>
2018-08-17 16:29:18 -0700

    Add dialixir support to check on code

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule ABI.Mixfile do
     [
       {:credo, "~>  0.9.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+        {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
       {:poison, "~> 3.1", only: [:dev, :test]},
       {:exth_crypto, "~> 0.1.4"}
     ]
